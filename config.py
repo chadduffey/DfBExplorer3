6ae3c67188159c07f1a6b352a90f0e3857d1e4b8
Chad Duffey <chadduffey@hotmail.com>
2015-05-17 18:11:12 +1000

    minor - removed static db path for heroku

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/DFB_DB'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'postgresql://localhost/DFB_DB'
 
 
 class TestingConfig(Config):
