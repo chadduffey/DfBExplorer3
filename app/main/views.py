99a0cbeebb42bcd653f500a3d2690b2ebae123f7
Chad Duffey <chadduffey@hotmail.com>
2015-05-19 14:44:44 +1000

    add api calls to main page, add graph library

--- app/main/views.py
+++ app/main/views.py
@@ -5,9 +5,26 @@ from ..models import User
 from ..email import send_email
 from . import main
 
+import urllib2
+import json
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
     if current_user.is_authenticated():
-    	return render_template('main/main.html', user=current_user)
+		#Dropbox Basics
+		request = urllib2.Request('https://api.dropbox.com/1/team/get_info')
+		request.add_header('Content-type', 'application/json')
+		request.add_header('Authorization','Bearer Roti84bECDUAAAAAAAACOhZz6DJtV9g4kWxk2_6gc7GOObK7nrmz1c4ITHm1wlOE')
+		body = str('{}')
+		request.add_data(body)
+		response = urllib2.urlopen(request)
+		data = response.read()
+		converted_data = json.loads(data)
+		dbTeamName = converted_data["name"]
+		totalLicences = converted_data["num_licensed_users"]
+		licencesInUse = converted_data["num_provisioned_users"]
+		teamId = converted_data["team_id"]
+		remainingLicences = totalLicences - licencesInUse
+		return render_template('main/main.html', user=current_user, dbTeamName=dbTeamName, remainingLicences=remainingLicences,
+    							totalLicences=totalLicences, licencesInUse=licencesInUse, teamId=teamId)
     return render_template('index.html')
---
+++ scratchpad.py
@@ -0,0 +1,27 @@
+#this is a scratchpad to be launched independently of the app itself to test app calls and string work
+
+import urllib2
+import json
+
+request = urllib2.Request('https://api.dropbox.com/1/team/get_info')
+
+request.add_header('Content-type', 'application/json')
+
+request.add_header('Authorization','Bearer Roti84bECDUAAAAAAAACOhZz6DJtV9g4kWxk2_6gc7GOObK7nrmz1c4ITHm1wlOE')
+
+body = str('{}')
+
+request.add_data(body)
+
+response = urllib2.urlopen(request)
+
+data = response.read()
+
+#use the JSON libraries to use the data a bit more neatly...
+converted_data = json.loads(data)
+
+#print it out, to prove, that we are smashing the API :)
+print "Team name is: " + converted_data["name"]
+print "The total number of licences is: " + str(converted_data["num_licensed_users"])
+print "The total number of licences in use: " + str(converted_data["num_provisioned_users"])
+print "The Dropbox team ID is: " + converted_data["team_id"]
