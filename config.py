47024df4eecc5aa136abed00ed4cd00b65c9d4ca
Chad Duffey <chadduffey@hotmail.com>
2015-05-30 12:00:28 +1000

    heroku issue

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'asdasd(*^&^$&%$'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
